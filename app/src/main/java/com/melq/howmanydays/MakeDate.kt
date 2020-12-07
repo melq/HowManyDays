dc6dc36d5c37f9e152d6d6a6db79cf187009e22a
melq <tu2sice@yahoo.co.jp>
2020-12-07 17:13:03 +0900

    :construction: 日付データ作成画面に必要な変数を作成

--- app/src/main/java/com/melq/howmanydays/MakeDate.kt
+++ app/src/main/java/com/melq/howmanydays/MakeDate.kt
@@ -2,6 +2,7 @@ package com.melq.howmanydays
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.time.LocalDate
@@ -12,19 +13,30 @@ class MakeDate : AppCompatActivity() {
         setContentView(R.layout.activity_makedate)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val tvCancel: TextView = findViewById(R.id.tv_cancel)
-        val tvMake: TextView = findViewById(R.id.tv_make)
+        var name: String
+        var year: Int = 1
+        var month: Int = 1
+        var date: Int = 1
 
+        val etSetName = findViewById<EditText>(R.id.et_name)
+        val tvSetDate = findViewById<TextView>(R.id.tv_set_date)
+        val tvCancel = findViewById<TextView>(R.id.tv_cancel)
+        val tvMake  = findViewById<TextView>(R.id.tv_make)
+
+        tvSetDate.setOnClickListener {
+            
+        }
         tvCancel.setOnClickListener {
             setResult(RESULT_CANCELED)
             finish()
         }
         tvMake.setOnClickListener {
+            name = etSetName.text.toString()
             val intent = Intent()
-            intent.putExtra("MakeDate.DateName", "myBirthDay")
-            intent.putExtra("MakeDate.Year", 1999)
-            intent.putExtra("MakeDate.Month", 8)
-            intent.putExtra("MakeDate.Date", 26)
+            intent.putExtra("MakeDate.DateName", name)
+            intent.putExtra("MakeDate.Year", year)
+            intent.putExtra("MakeDate.Month", month)
+            intent.putExtra("MakeDate.Date", date)
             setResult(RESULT_OK, intent)
             finish()
         }
